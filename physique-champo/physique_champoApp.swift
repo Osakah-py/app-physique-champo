0adffe3d6778e77935d51cc45e1b95cc2c877c7f
alex <lescot.alexg@gmail.com>
2024-07-12 19:50:37 +0200

    Initial Commit

---
+++ physique-champo/physique_champoApp.swift
@@ -0,0 +1,17 @@
+//
+//  physique_champoApp.swift
+//  physique-champo
+//
+//  Created by Alex Lescot on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct physique_champoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ physique-champoTests/physique_champoTests.swift
@@ -0,0 +1,17 @@
+//
+//  physique_champoTests.swift
+//  physique-champoTests
+//
+//  Created by Alex Lescot on 12/07/2024.
+//
+
+import Testing
+@testable import physique_champo
+
+struct physique_champoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
